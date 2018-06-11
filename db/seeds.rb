9fe1d7276e7c23fac7cbaf9516d142030a9cff26
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-11 14:26:00 -0400

    Solved lab

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create!(title: "Hey Jude" , released: true , release_year: 1965, artist_name: "The Beatles", genre: "pop")
+Song.create!(title: "New York, New York" , released: true , release_year: 1970, artist_name: "Frank Sinatra", genre: "melodic")
