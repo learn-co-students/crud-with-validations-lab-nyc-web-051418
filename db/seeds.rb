47ceb5e2a8b67fcb5822db397648582769fda6bf
github username <github email address>
2018-06-12 14:10:52 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+drive = Song.create(title: "Drive", released: false, artist_name: "Lil uzi vert", genre: "Rap")
+xo = Song.create(title: "XO Tour Llif3", released: false, artist_name: "Lil uzi vert", genre: "Rap")
